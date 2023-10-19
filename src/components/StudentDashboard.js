caadf4283c37c6f445866ba5c866c6a579725835
LeilaNsm <115103073+LeilaNsm@users.noreply.github.com>
2023-10-19 09:42:08 -0500

    Updated StudentDashboard.js

--- src/components/StudentDashboard.js
+++ src/components/StudentDashboard.js
@@ -1,6 +1,8 @@
 // StudentDashboard.js
 
 import React, { useState } from 'react';
+import { useParams, Link } from 'react-router-dom';
+import Button from '@mui/material/Button';
 import Table from 'react-bootstrap/Table';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
@@ -10,22 +12,31 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
+
 function StudentDashboard() {
-  return (
-    <>
-      <Navbar bg="dark" data-bs-theme="dark">
-        <Container>
-          <Navbar.Brand href="#home">Student Dashboard</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">My favorites</Nav.Link>
-            <Nav.Link href="#features">My Schedule</Nav.Link>
-        
-          </Nav>
-        </Container>
-      </Navbar> 
-    </>
-  );
+
+    return (
+        <div>
+            <h3>Student Dashboard</h3>
+            <Link to="/StudentMyFavorites" style={{ textDecoration: 'none', marginRight: '16px' }}>
+                <Button
+                    style={{ backgroundColor: 'yellow', color: 'black' }}
+                >
+                    My Favorits
+                </Button>
+            </Link>
+            <Link to="/StudentMySchedule" style={{ textDecoration: 'none' }}>
+                <Button
+                    style={{ backgroundColor: 'pink', color: 'black' }}
+                >
+                    My Schedule
+                </Button>
+            </Link>
+        </div>
+    );
+
 }
+
 
 
 
