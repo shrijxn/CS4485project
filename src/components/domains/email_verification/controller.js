ee00e6d5e9f098d1f557067afc169f40c2a6cdc2
Kay Takamatsu <silverweek54@gamil.com>
2023-11-17 14:51:22 -0600

    11/17

---
+++ src/components/domains/email_verification/controller.js
@@ -0,0 +1,28 @@
+const User = require("./../user/model");
+const { sendOTP } = require("./../otp/controller");
+
+const sendVerificationOTPEmail = async (email) => {
+    try {
+        // check if account exist    
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            throw Error("There's no accont for the provided email.");
+
+        }
+
+        const otpDetails = {
+        email,
+        subject: "Email Verification",
+        message: "Verify your email with the code below.",
+        duration: 1,
+        };
+        const createdOTP = await sendOTP(otpDetails);
+        return createdOTP;
+    } catch (error) {
+        throw error;
+    }
+};
+
+module.exports = { sendVerificationOTPEmail };
+
+
