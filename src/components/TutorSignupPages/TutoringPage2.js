a66e0e686e392672e7abf2824d4d55c587895fc0
LeilaNsm <115103073+LeilaNsm@users.noreply.github.com>
2023-11-20 10:04:48 -0600

    Update TutoringPage2.js

--- src/components/TutorSignupPages/TutoringPage2.js
+++ src/components/TutorSignupPages/TutoringPage2.js
@@ -2,42 +2,57 @@
 
 import React from "react";
 import { useFormikContext, Field, ErrorMessage } from "formik";
+import "../../App.css";
 
 function Page2() {
   const { errors, touched } = useFormikContext();
   return (
     <div>
       <div>
-        <label htmlFor="phone">Phone (Optional): </label>
-        <Field type="text" id="phone" name="phone" />
+        <label htmlFor="phone" className="animated-text">
+          Phone (Optional):{" "}
+        </label>
+        <Field type="text" id="phone" name="phone" className="formik-input" />
         <ErrorMessage name="phone" component="div" className="error-message" />
       </div>
       <div>
-        <label htmlFor="street">Street: </label>
-        <Field type="text" id="street" name="street" />
+        <label htmlFor="street" className="animated-text">
+          Street:{" "}
+        </label>
+        <Field type="text" id="street" name="street" className="formik-input" />
         <ErrorMessage name="street" component="div" className="error-message" />
       </div>
       <div>
-        <label htmlFor="city">City: </label>
-        <Field type="text" id="city" name="city" />
+        <label htmlFor="city" className="animated-text">
+          City:{" "}
+        </label>
+        <Field type="text" id="city" name="city" className="formik-input" />
         <ErrorMessage name="city" component="div" className="error-message" />
       </div>
       <div>
-        <label htmlFor="state">State: </label>
-        <Field type="text" id="state" name="state" />
+        <label htmlFor="state" className="animated-text">
+          State:{" "}
+        </label>
+        <Field type="text" id="state" name="state" className="formik-input" />
         <ErrorMessage name="state" component="div" className="error-message" />
       </div>
       <div>
-        <label htmlFor="zip">Zip Code: </label>
-        <Field type="text" id="zip" name="zip" />
+        <label htmlFor="zip" className="animated-text">
+          Zip Code:{" "}
+        </label>
+        <Field type="text" id="zip" name="zip" className="formik-input" />
         <ErrorMessage name="zip" component="div" className="error-message" />
       </div>
       <div>
-        <label>Do you have a criminal record?</label>
+        <label className="criminal-text">Do you have a criminal record?</label>
         <Field type="radio" name="criminalRecord" value="yes" id="yes" />
-        <label htmlFor="yes">Yes</label>
+        <label htmlFor="yes" className="criminal-text">
+          Yes
+        </label>
         <Field type="radio" name="criminalRecord" value="no" id="no" />
-        <label htmlFor="no">No</label>
+        <label htmlFor="no" className="option-text">
+          No
+        </label>
         <ErrorMessage
           name="criminalRecord"
           component="div"
