b230c349e173f3c6a60dad93d9d3170c7215c154
LeilaNsm <115103073+LeilaNsm@users.noreply.github.com>
2023-11-20 10:05:43 -0600

    Update TutorDashboard.js

--- src/components/TutorDashboard.js
+++ src/components/TutorDashboard.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import * as Icon from 'react-bootstrap-icons';
-import { Link } from 'react-router-dom'; 
+// TutorDashboard.js
+
+import React from "react";
+import * as Icon from "react-bootstrap-icons";
+import { Link } from "react-router-dom";
 
 function TutorDashboard() {
-  return( 
-    <>
-      <h2>Tutor Dashboard</h2>
-      <div>Total Hours: </div>
+  return (
+    <div>
+      <h2 className="header-animated-text" style={{ marginRight: "450px" }}>
+        Tutor Dashboard
+      </h2>
+      <div className="header2-animated-text" style={{ marginRight: "450px" }}>
+        Total Hours:{" "}
+      </div>
       <Link to="/TutorCalendar">
-        <Icon.CalendarWeek size={200} style={{marginTop: '30px', color: 'white'}} />
+        <Icon.CalendarWeek
+          size={200}
+          style={{ marginRight: "750px" }}
+          className="header-animated-text"
+        />
       </Link>
-    </>
+    </div>
   );
 }
 
