b5985e6ae47b1451c110f0f6cf81e71826f42279
Kay Takamatsu <silverweek54@gamil.com>
2023-11-27 13:39:56 -0600

    email ver

--- src/components/routes/index.js
+++ src/components/routes/index.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const userRoutes = require("./../domains/user");
 const OTPRoutes = require("./../domains/otp");
+const EmailVerificationRoutes = require("./../domains/email_verification");
 
 router.use("/user", userRoutes);
 router.use("/otp", OTPRoutes);
-
+router.use("/email_verification", EmailVerificationRoutes);
 module.exports = router;
 
