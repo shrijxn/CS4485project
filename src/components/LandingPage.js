6b99dc7469e38ee206cec5966fbb065ee1cce22b
LeilaNsm <115103073+LeilaNsm@users.noreply.github.com>
2023-11-28 16:12:40 -0600

    Update LandingPage.js

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,27 +1,86 @@
-import React from 'react';
-import { useParams, Link } from 'react-router-dom';
-import Button from '@mui/material/Button';
+// LandingPage.js
+
+import React from "react";
+import { useParams, Link } from "react-router-dom";
+import Button from "@mui/material/Button";
+import NewUserImage from "../logo/NewUser.png";
+import ReturningUserImage from "../logo/ReturningUser.png";
+import { createTheme } from "@mui/system";
+
+// Define the theme
+const defaultTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#c7b94c",
+    },
+    secondary: {
+      main: "#afc782",
+    },
+  },
+});
 
 function LandingPage() {
-    const { type } = useParams(); // will be either 'student' or 'tutor'
+  const { type } = useParams(); // will be either 'student' or 'tutor'
+  const typeCapitalized = type.charAt(0).toUpperCase() + type.slice(1);
 
-    return (
-        <div>
-            <h2>Welcome to the {type.charAt(0).toUpperCase() + type.slice(1)} Section</h2>
-            <p>Select your option below:</p>
-            <Link to={`/${type}Signup`} style={{ textDecoration: 'none', marginRight: '16px' }}>
-                <Button variant="contained" color="primary">
-                    New {type.charAt(0).toUpperCase() + type.slice(1)}
-                </Button>
-            </Link>
-            <Link to={`/${type}Login`} style={{ textDecoration: 'none' }}>
-                <Button variant="contained" color="secondary">
-                    Returning {type.charAt(0).toUpperCase() + type.slice(1)}
-                </Button>
-            </Link>
-        </div>
-        
-    );
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h2 className="header-animated-text">
+        Welcome to the {typeCapitalized} Section
+      </h2>
+      <p className="header2-animated-text">Select your option below:</p>
+      <Link
+        to={`/${type}Signup`}
+        style={{ textDecoration: "none", marginRight: "32px" }}
+      >
+        <Button
+          startIcon={
+            <img
+              src={NewUserImage}
+              alt="New User"
+              style={{ width: "100px", height: "100px" }}
+            />
+          }
+          variant="outlined"
+          sx={{
+            color: "white",
+            borderColor: "white",
+            "&:hover": {
+              backgroundColor: defaultTheme.palette.primary.main,
+              borderColor: defaultTheme.palette.primary.main,
+            },
+          }}
+        >
+          New {typeCapitalized}
+        </Button>
+      </Link>
+      <Link
+        to={`/${type}Login`}
+        style={{ textDecoration: "none", marginRight: "650px" }}
+      >
+        <Button
+          endIcon={
+            <img
+              src={ReturningUserImage}
+              alt="Returning User"
+              style={{ width: "100px", height: "100px" }}
+            />
+          }
+          variant="outlined"
+          sx={{
+            color: "white",
+            borderColor: "white",
+            "&:hover": {
+              backgroundColor: defaultTheme.palette.secondary.main,
+              borderColor: defaultTheme.palette.secondary.main,
+            },
+          }}
+        >
+          Returning {typeCapitalized}
+        </Button>
+      </Link>
+    </div>
+  );
 }
 
 export default LandingPage;
