6ea868aed1352514c1fa8d4bdb9b66ce6388ca4e
LeilaNsm <115103073+LeilaNsm@users.noreply.github.com>
2023-12-03 13:32:00 -0600

    Update TutoringPage1.js

--- src/components/TutorSignupPages/TutoringPage1.js
+++ src/components/TutorSignupPages/TutoringPage1.js
@@ -1,11 +1,33 @@
 // TutoringPage1.js
 
-import React from "react";
+import React, { useState } from "react";
 import { useFormikContext, Field, ErrorMessage } from "formik";
 import "../../App.css";
 
 function Page1() {
-  const { errors, touched } = useFormikContext();
+  const { errors, touched, values, setFieldError, setFieldValue } =
+    useFormikContext();
+
+  // State for image management
+  const [image, setImage] = useState(null);
+  const [imageName, setImageName] = useState("");
+
+  // Handle image change
+  const handleImageChange = (e) => {
+    if (e.target.files && e.target.files[0]) {
+      const newImageName = `${values.firstName}.${values.lastName}`;
+      setImageName(newImageName); // Update the image name state
+      setImage(URL.createObjectURL(e.target.files[0]));
+      setFieldValue(
+        "photo",
+        new Blob([e.target.files[0]], { type: "image/png" }),
+        newImageName
+      );
+      setFieldError("photo", ""); // Clear any previous error
+    } else {
+      setFieldError("photo", "Profile photo is required");
+    }
+  };
 
   return (
     <div>
@@ -90,6 +112,40 @@ function Page1() {
           component="div"
           className="error-message"
         />
+      </div>
+
+      {/* Image upload section */}
+      <div style={{ marginRight: "600px" }}>
+        <label htmlFor="photo" className="animated-text file-input-text">
+          Upload Photo:
+        </label>
+        <input
+          type="file"
+          id="photo"
+          name="photo"
+          accept="image/*"
+          className="file-input"
+          onChange={handleImageChange}
+        />
+        <label htmlFor="photo" className="file-input-label">
+          Choose File
+        </label>
+
+        {image && (
+          <div style={{ marginTop: "5px", marginBottom: "5px" }}>
+            <span className="file-name">{imageName}</span>{" "}
+            <img
+              src={image}
+              alt="Preview"
+              style={{
+                width: "100px",
+                height: "auto",
+                border: "1px solid white",
+              }}
+            />
+          </div>
+        )}
+        <ErrorMessage name="photo" component="div" className="error-message" />
       </div>
     </div>
   );
