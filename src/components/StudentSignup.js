dd07533ebe9650bb9e987dcd953ad58d2db0f67f
LeilaNsm <115103073+LeilaNsm@users.noreply.github.com>
2023-12-03 10:34:25 -0600

    Update StudentSignup.js

--- src/components/StudentSignup.js
+++ src/components/StudentSignup.js
@@ -1,13 +1,15 @@
 // StudentSignup.js
 
-import React from "react";
+import React, { useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import { useNavigate } from "react-router-dom";
 import "../App.css";
 
 function StudentSignup() {
-  console.log("Rendering Signup Component....");
+  const [image, setImage] = useState(null);
+  const [imageName, setImageName] = useState("");
+  const [imageError, setImageError] = useState("");
   const navigate = useNavigate();
 
   const initialValues = {
@@ -18,13 +20,14 @@ function StudentSignup() {
     phone: "",
     password: "",
     confirmPassword: "",
+    imageName: "",
   };
 
   const validationSchema = Yup.object({
     firstName: Yup.string().required("First Name is required"),
     lastName: Yup.string().required("Last Name is required"),
     email: Yup.string()
-      .matches(/^[^@]+@[^@]+\.[a-zA-Z]{3}$/, "Invalid email format")
+      .email("Invalid email format")
       .required("Email is required"),
     phone: Yup.string().matches(
       /^\d{10}$/,
@@ -39,14 +42,25 @@ function StudentSignup() {
       ),
     confirmPassword: Yup.string()
       .required("Confirm Password is required")
-      .oneOf(
-        [Yup.ref("password"), null],
-        "Password confirmation does not match"
-      ),
+      .oneOf([Yup.ref("password"), null], "Passwords must match"),
   });
 
-  const handleLogin = (values) => {
+  const handleImageChange = (e, setFieldValue, currentValues) => {
+    if (e.target.files[0]) {
+      const newImageName = `${currentValues.firstName}.${currentValues.lastName}`;
+      setImage(URL.createObjectURL(e.target.files[0]));
+      setImageName(newImageName); // Set image name based on current firstName and lastName
+      setFieldValue("imageName", newImageName);
+      setImageError("");
+    }
+  };
+  const handleLogin = (values, { setSubmitting }) => {
     console.log("Form data submitted:", values);
+    if (!image) {
+      setImageError("Profile photo is required");
+      setSubmitting(false);
+      return;
+    }
     if (
       values.firstName !== "" &&
       values.lastName !== "" &&
@@ -71,6 +85,8 @@ function StudentSignup() {
         });
       navigate("/StudentDashboard");
     }
+
+    setSubmitting(false);
   };
 
   return (
@@ -81,118 +97,161 @@ function StudentSignup() {
         validationSchema={validationSchema}
         onSubmit={handleLogin}
       >
-        <Form>
-          <div>
-            <label htmlFor="firstName" className="animated-text">
-              First Name{" "}
-            </label>
-            <Field
-              type="text"
-              id="firstName"
-              name="firstName"
-              className="formik-input"
-            />
-            <ErrorMessage
-              name="firstName"
-              component="div"
-              className="error-message"
-            />
-          </div>
-          <div>
-            <label htmlFor="middleName" className="animated-text">
-              Middle Name (Optional){" "}
-            </label>
-            <Field
-              type="text"
-              id="middleName"
-              name="middleName"
-              className="formik-input"
-            />
-          </div>
-          <div>
-            <label htmlFor="lastName" className="animated-text">
-              Last Name{" "}
-            </label>
-            <Field
-              type="text"
-              id="lastName"
-              name="lastName"
-              className="formik-input"
-            />
-            <ErrorMessage
-              name="lastName"
-              component="div"
-              className="error-message"
-            />
-          </div>
-          <div>
-            <label htmlFor="email" className="animated-text">
-              Email{" "}
-            </label>
-            <Field
-              type="email"
-              id="email"
-              name="email"
-              className="formik-input"
-            />
-            <ErrorMessage
-              name="email"
-              component="div"
-              className="error-message"
-            />
-          </div>
-          <div>
-            <label htmlFor="phone" className="animated-text">
-              Phone (Optional){" "}
-            </label>
-            <Field
-              type="text"
-              id="phone"
-              name="phone"
-              className="formik-input"
-            />
-            <ErrorMessage
-              name="phone"
-              component="div"
-              className="error-message"
-            />
-          </div>
-          <div>
-            <label htmlFor="password" className="animated-text">
-              Password{" "}
-            </label>
-            <Field
-              type="password"
-              id="password"
-              name="password"
-              className="formik-input"
-            />
-            <ErrorMessage
-              name="password"
-              component="div"
-              className="error-message"
-            />
-          </div>
-          <div>
-            <label htmlFor="confirmPassword" className="animated-text">
-              Confirm Password{" "}
-            </label>
-            <Field
-              type="password"
-              id="confirmPassword"
-              name="confirmPassword"
-              className="formik-input"
-            />
-            <ErrorMessage
-              name="confirmPassword"
-              component="div"
-              className="error-message"
-            />
-          </div>
-          <button type="submit" className="next-button-student">
-            Sign up
-          </button>
-        </Form>
+        {({ isSubmitting, setFieldValue, values }) => (
+          <Form>
+            <div>
+              <label htmlFor="firstName" className="animated-text">
+                First Name
+              </label>
+              <Field
+                type="text"
+                id="firstName"
+                name="firstName"
+                className="formik-input"
+              />
+              <ErrorMessage
+                name="firstName"
+                component="div"
+                className="error-message"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="middleName" className="animated-text">
+                Middle Name (Optional){" "}
+              </label>
+              <Field
+                type="text"
+                id="middleName"
+                name="middleName"
+                className="formik-input"
+              />
+            </div>
+            <div>
+              <label htmlFor="lastName" className="animated-text">
+                Last Name{" "}
+              </label>
+              <Field
+                type="text"
+                id="lastName"
+                name="lastName"
+                className="formik-input"
+              />
+              <ErrorMessage
+                name="lastName"
+                component="div"
+                className="error-message"
+              />
+            </div>
+            <div>
+              <label htmlFor="email" className="animated-text">
+                Email{" "}
+              </label>
+              <Field
+                type="email"
+                id="email"
+                name="email"
+                className="formik-input"
+              />
+              <ErrorMessage
+                name="email"
+                component="div"
+                className="error-message"
+              />
+            </div>
+            <div>
+              <label htmlFor="phone" className="animated-text">
+                Phone (Optional){" "}
+              </label>
+              <Field
+                type="text"
+                id="phone"
+                name="phone"
+                className="formik-input"
+              />
+              <ErrorMessage
+                name="phone"
+                component="div"
+                className="error-message"
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="animated-text">
+                Password{" "}
+              </label>
+              <Field
+                type="password"
+                id="password"
+                name="password"
+                className="formik-input"
+              />
+              <ErrorMessage
+                name="password"
+                component="div"
+                className="error-message"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="confirmPassword" className="animated-text">
+                Confirm Password
+              </label>
+              <Field
+                type="password"
+                id="confirmPassword"
+                name="confirmPassword"
+                className="formik-input"
+              />
+              <ErrorMessage
+                name="confirmPassword"
+                component="div"
+                className="error-message"
+              />
+            </div>
+
+            <div style={{ marginRight: "600px" }}>
+              <label htmlFor="photo" className="animated-text file-input-text">
+                Upload Photo
+              </label>
+
+              <input
+                type="file"
+                id="photo"
+                name="photo"
+                accept="image/*"
+                className="file-input"
+                onChange={(e) => handleImageChange(e, setFieldValue, values)}
+              />
+              <label htmlFor="photo" className="file-input-label">
+                Choose File
+              </label>
+
+              {image && (
+                <div style={{ marginTop: "5px", marginBottom: "5px" }}>
+                  <span className="file-name">{imageName}</span>{" "}
+                  <img
+                    src={image}
+                    alt="Preview"
+                    style={{
+                      width: "100px",
+                      height: "auto",
+                      border: "1px solid white",
+                    }}
+                  />
+                </div>
+              )}
+              {imageError && <div className="error-message">{imageError}</div>}
+            </div>
+
+            <button
+              type="submit"
+              disabled={isSubmitting}
+              className="next-button-student"
+            >
+              Sign up
+            </button>
+          </Form>
+        )}
       </Formik>
     </div>
   );
